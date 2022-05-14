a43a066a1c0f029dc2d8fca0b219ed144eeb35e2
Adam Wride <ajwride1@gmail.com>
2022-05-14 23:31:56 +0100

    new endpoints created to implement the remaining crud operations

--- Controllers/PizzaController.cs
+++ Controllers/PizzaController.cs
@@ -27,5 +27,39 @@ public class PizzaController : ControllerBase
         return pizza;
     }
 
+    [HttpPost]
+    public IActionResult Create(Pizza pizza)
+    {
+        PizzaService.Add(pizza);
+        return CreatedAtAction(nameof(Create), new { id = pizza.Id }, pizza);
+    }
 
+    [HttpPut("{id}")]
+    public IActionResult Update(int id, Pizza pizza)
+    {
+        if(id != pizza.Id)
+            return BadRequest();
+
+        Pizza? existingPizza = PizzaService.Get(id);
+        
+        if(existingPizza == null)
+            return NotFound();
+
+        PizzaService.Update(pizza);
+
+        return NoContent();
+    }
+
+    [HttpDelete("{id}")]
+    public IActionResult Delete(int id)
+    {
+        Pizza? pizza = PizzaService.Get(id);
+
+        if(pizza == null)
+            return NotFound();
+
+        PizzaService.Delete(id);
+
+        return NoContent();
+    }
 }
